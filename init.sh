757af4099909de2bebd813bc3f4e3faae6c706be
root <root@debian.home>
2016-03-23 04:15:22 +0600

    Рабочий конфиг

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-service nginx restart
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo service nginx restart
