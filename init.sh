9395ece1a696f8ba2eaf2759c72e6ffa454a2bd8
meanmail <meanmail@mail.ru>
2016-03-24 22:14:50 +0600

    Добавлен WSGI hello. Конфигурация Gunicorn. Дополнен init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
-ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-service nginx restart
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo service nginx restart
+cd /hello/box/web
+mkdir uploads public
+cd public
+mkdir css img js
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo service gunicorn restart
