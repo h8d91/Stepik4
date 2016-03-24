80a8b06ca37273cb8c02c7b2352e350b6f1aec8b
meanmail <meanmail@mail.ru>
2016-03-24 22:33:09 +0600

    Исправлена ошибка

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo service nginx restart
-cd /hello/box/web
+cd /home/box/web
 mkdir uploads public
 cd public
 mkdir css img js
