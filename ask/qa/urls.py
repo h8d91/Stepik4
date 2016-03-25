901084c2427cd51e468ec45b9af7a19f30ed1db8
meanmail <meanmail@mail.ru>
2016-03-25 07:30:30 +0600

    edited urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('', 
-    url(r'^login/$', 'qa.views.login', name='login'),
-    url(r'^signup/$', 'qa.views.signup', name='signup'),
-    url(r'^question/\w+/$', 'qa.views.question', name='question'),
-    url(r'^ask/$', 'qa.views.ask', name='ask'),
-    url(r'^popular/$', 'qa.views.popular', name='popular'),
-    url(r'^new/$', 'qa.views.new', name='new'),
+    url(r'^login/.*$', 'qa.views.login', name='login'),
+    url(r'^signup/.*$', 'qa.views.signup', name='signup'),
+    url(r'^question/\d+/.*$', 'qa.views.question', name='question'),
+    url(r'^ask/.*$', 'qa.views.ask', name='ask'),
+    url(r'^popular/.*$', 'qa.views.popular', name='popular'),
+    url(r'^new/.*$', 'qa.views.new', name='new'),
     url(r'^$', 'qa.views.home', name='home'),
 )
