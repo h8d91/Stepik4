d9079c7352c5f6f3750fb19bbd0288e8423e537d
meanmail <meanmail@mail.ru>
2016-03-25 06:33:10 +0600

    all views return 200 OK

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,30 +1,27 @@
-from django.http import HttpResponse, HttpRequest, Http404
+from django.http import HttpResponse, HttpRequest
 
 def test(request, *args, **kwargs):
-	return HttpResponse('OK')
-
-def http404():
-	raise Http404
+        return HttpResponse('OK')
 
 def login(request, *args, **kwargs):
-	return http404()
+        return test(request, args, kwargs)
 
 def signup(request, *args, **kwargs):
-        return http404()
+        return test(request, args, kwargs)
 
 def question(request, *args, **kwargs):
-        return HttpResponse(request.get_full_path())
+        return test(request, args, kwargs)
 
 def ask(request, *args, **kwargs):
-        return http404()
+        return test(request, args, kwargs)
 
 def popular(request, *args, **kwargs):
-        return http404()
+        return test(request, args, kwargs)
 
 def new(request, *args, **kwargs):
-        return http404()
+        return test(request, args, kwargs)
 
 def home(request, *args, **kwargs):
-        return http404()
+        return test(request, args, kwargs)
 
 
