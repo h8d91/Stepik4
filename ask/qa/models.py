8a1cd9c01953f2dca3c3c3257a9344e8a8e138ca
meanmail <meanmail@mail.ru>
2016-03-27 13:54:58 +0600

    Импорт модели пользователя

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField()
     rating = models.IntegerField()
-    author = models.ForeignKey('django.contrib.auth.User')
-    likes = models.ManyToManyField('django.contrib.auth.User')
+    author = models.ForeignKey('User')
+    likes = models.ManyToManyField('User')
     
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField()
     question = models.ForeignKey('Question')
-    author = models.ForeignKey('django.contrib.auth.User')
+    author = models.ForeignKey('User')
