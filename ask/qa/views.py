248c9c2a3d7069e0d289cd57e42de6fbd7e9e802
meanmail <meanmail@mail.ru>
2016-03-27 22:01:25 +0600

    Подправлен контроллер question

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -20,8 +20,10 @@ def question(request, *args, **kwargs):
         except:
             raise Http404
         
+        answers = Answers.objects.filter('question'=question).order_by('-added_at').all()[:]
+        
         return render(request, 'question.html', {
-                'answers': Answers.objects.filter('question'=question).order_by('-added_at').all(),
+                'answers': answers,
                 'question': question,
         })
 
