911d2bcf6c18abb48aa8595856309428ed83dd0f
meanmail <meanmail@mail.ru>
2016-03-27 22:07:37 +0600

    Импорт Answer в контроллер

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpRequest, Http404
 from django.core.paginator import Paginator, EmptyPage
-from qa.models import Question
+from qa.models import Question, Answer
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 
@@ -20,7 +20,7 @@ def question(request, *args, **kwargs):
         except:
             raise Http404
         
-        answers = Answers.objects.filter(question = question).order_by('-added_at').all()[:]
+        answers = Answer.objects.filter(question = question).order_by('-added_at').all()[:]
         
         return render(request, 'question.html', {
                 'answers': answers,
