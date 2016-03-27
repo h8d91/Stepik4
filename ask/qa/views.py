e5d2c836e5f31f95c9fd6201882c464dd478f973
meanmail <meanmail@mail.ru>
2016-03-27 20:28:27 +0600

    import http404

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, http404
 from django.core.paginator import Paginator, EmptyPage
 from qa.models import Question
 from django.shortcuts import render
