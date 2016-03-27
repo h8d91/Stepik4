6cf41673c71c802cdd30eeceefaa67903ee12f6f
meanmail <meanmail@mail.ru>
2016-03-27 22:35:04 +0600

    "Column 'added_at' cannot be null"

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(blank=False, null=False)
     rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_users')
@@ -18,6 +18,6 @@ class Question(models.Model):
     
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(blank=False, null=False)
     question = models.ForeignKey('Question')
     author = models.ForeignKey(User)
