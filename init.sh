9aa2d1c41b3257be28d089d28202ca5635348461
meanmail <meanmail@mail.ru>
2016-03-27 20:03:14 +0600

    Изменены скрипты

--- init.sh
+++ init.sh
@@ -1,15 +1,19 @@
+cd /home/box/web
+
 sudo mysql -uroot -e "create database myproject;"
 
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo service nginx restart
 
-cd /home/box/web
 mkdir uploads public
 cd public
 mkdir css img js
 
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
-sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
-sudo service gunicorn restart
+cd /home/box/web
+sudo ln -s etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo ln -s etc/ask.py /etc/gunicorn.d/ask.py
 
+python ask/manage.py makemigrations
+python ask/manage.py migrate
+
+bash restartServers.sh
---
+++ restartServers.sh
@@ -0,0 +1,5 @@
+echo "Restart NGINX"
+sudo service nginx restart
+
+echo "Restart Gunicorn"
+sudo service gunicorn restart
