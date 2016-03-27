5a10a3f03e0c526546b280bf6c98e09a2460a03c
meanmail <meanmail@mail.ru>
2016-03-27 22:49:54 +0600

    added_at auto_now

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField(blank=False, null=False)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_users')
@@ -18,6 +18,6 @@ class Question(models.Model):
     
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(blank=False, null=False)
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey('Question')
     author = models.ForeignKey(User)
