1a39704d7986446ae46a35a2e54db4ec795207a7
meanmail <meanmail@mail.ru>
2016-03-29 01:58:22 +0600

    Не пойму в чем дело, добавил отступов

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -12,16 +12,16 @@ class AskForm(forms.Form):
         title = self.cleaned_data.get('title')
         if len(title) < 2:
             raise ValidateError(u'Заголовок должен быть больше одного символа')
-
+        
         return title
-
+        
     def clean_text(self):
         text = self.cleaned_data.get('text')
         if len(text) < 2:
             raise ValidateError(u'Тект должен быть больше одного символа')
-
+        
         return text
-
+        
     def save(self):
         question = Question()
         question.title = self.cleaned_data.get('title')
@@ -32,22 +32,22 @@ class AskForm(forms.Form):
 class AnswerForm(forms.Form):
     text = forms.CharField(label=u'Ваш вопрос', widget=forms.TextInput)
     question = forms.IntegerField(widget=forms.HiddenInput)
-
+    
     def clean(self):
         try:
             Ouestion.objects.get(id=self.cleaned_data.get('question'))
         except:
             raise ValidateError(u'Вопрос на который вы отвечаете не существует или удалён')
-
+         
         return True 
-
+         
     def clean_text(self):
         text = self.cleaned_data.get('text')
         if len(text) < 2:
             raise ValidateError(u'Тект должен быть больше одного символа')
-
+         
         return text
-
+        
     def save(self):
         answer = Answer()
         answer.text = self.cleaned_data.get('text')
