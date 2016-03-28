bb80bb77cec659e285ef19baa6f1c9ec0bb3d30c
meanmail <meanmail@mail.ru>
2016-03-29 00:21:50 +0600

    form -> forms

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -2,7 +2,7 @@
 
 class AskForm(forms.Form):
     title = forms.CharField(label=u'Заголовок', max_length=255)
-    text = form.TextField(label=u'Текст вопроса', widget=forms.TextArea)
+    text = forms.TextField(label=u'Текст вопроса', widget=forms.TextArea)
     
     def clean(self):
         return True
@@ -25,8 +25,8 @@ class AskForm(forms.Form):
         return question
 
 class AnswerForm(forms.Form):
-    text = form.TextField(label=u'Ваш вопрос', widget=forms.TextArea)
-    question = form.IntegerField(visible=False)
+    text = forms.TextField(label=u'Ваш вопрос', widget=forms.TextArea)
+    question = forms.IntegerField(visible=False)
 
     def clean(self):
         try:
