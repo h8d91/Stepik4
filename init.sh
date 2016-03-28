8e53c86bdea10682c59d0886503e02780ea5ca75
meanmail <meanmail@mail.ru>
2016-03-28 21:33:19 +0600

    Убрано создание каталогов из скрипта

--- init.sh
+++ init.sh
@@ -5,10 +5,6 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 
 cd /home/box/web
-mkdir uploads public
-cd public
-mkdir css img js
-
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 
