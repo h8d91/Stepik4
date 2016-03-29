1b3d8fbc661360249c5b0bc32f02d956eaaa0d19
meanmail <meanmail@mail.ru>
2016-03-29 08:03:34 +0600

    Подправлен скрипт

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ $1 = "stepic" ]
+if ( $1 = "stepic" )
 then sudo service mysql restart
 fi
 
 sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS myproject CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
-if [-e /etc/nginx/sites-enabled/test.conf]; then sudo rm /etc/nginx/sites-enabled/test.conf; fi
+if test -e /etc/nginx/sites-enabled/test.conf; then sudo rm /etc/nginx/sites-enabled/test.conf; fi
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-if [-e /etc/nginx/sites-enabled/default]; then sudo rm /etc/nginx/sites-enabled/default; fi
+if test -e /etc/nginx/sites-enabled/default; then sudo rm /etc/nginx/sites-enabled/default; fi
 
-if [-e /etc/gunicorn.d/hello.py]; then sudo rm /etc/gunicorn.d/hello.py; fi
-if [-e /etc/gunicorn.d/ask.py]; then sudo rm /etc/gunicorn.d/ask.py; fi
+if test -e /etc/gunicorn.d/hello.py; then sudo rm /etc/gunicorn.d/hello.py; fi
+if test -e /etc/gunicorn.d/ask.py; then sudo rm /etc/gunicorn.d/ask.py; fi
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 
